f2fd56cd7ea5fbbc936aa9c35aa17d6d062f2d8e
Anitra <griffinanitraa@johndeere.com>
2021-08-08 20:16:33 -0500

    Day 2 Final

--- server.js
+++ server.js
@@ -1,77 +1,87 @@
-const express = require('express');
-const app = express();//app is an object
+const express = require("express");
+const methodOverride = require("method-override");
+const app = express();
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  let logStr = `${req.method} ${req.url}`;
+
+  if (Object.keys(req.body).length !== 0) {
+    logStr += ` -- DATA: ${JSON.stringify(req.body)}`;
+  }
+
+  console.log(logStr);
+  next();
+});
 
 const pokemon = [
-    {
-      name: "Bulbasaur",
-      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
-    },
-    {
-      name: "Ivysaur",
-      img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
-    },
-    {
-      name: "Venusaur",
-      img: "http://img.pokemondb.net/artwork/venusaur.jpg"
-    },
-    {
-      name: "Charmander",
-      img: "http://img.pokemondb.net/artwork/charmander.jpg"
-    },
-    {
-      name: "Charizard",
-      img: "http://img.pokemondb.net/artwork/charizard.jpg"
-    },
-    {
-      name: "Squirtle",
-      img: "http://img.pokemondb.net/artwork/squirtle.jpg"
-    },
-    {
-      name: "Wartortle",
-      img: "http://img.pokemondb.net/artwork/wartortle.jpg"
-    }
-  ];
+  {
+    name: "Bulbasaur",
+    img: "http://img.pokemondb.net/artwork/bulbasaur.jpg",
+  },
+  {
+    name: "Ivysaur",
+    img: "http://img.pokemondb.net/artwork/ivysaur.jpg",
+  },
+  {
+    name: "Venusaur",
+    img: "http://img.pokemondb.net/artwork/venusaur.jpg",
+  },
+  {
+    name: "Charmander",
+    img: "http://img.pokemondb.net/artwork/charmander.jpg",
+  },
+  {
+    name: "Charizard",
+    img: "http://img.pokemondb.net/artwork/charizard.jpg",
+  },
+  {
+    name: "Squirtle",
+    img: "http://img.pokemondb.net/artwork/squirtle.jpg",
+  },
+  {
+    name: "Wartortle",
+    img: "http://img.pokemondb.net/artwork/wartortle.jpg",
+  },
+];
 
-//index
-app.get('/pokemon', (req, res) => {
-  res.render('index.ejs', {
-      pokemon: pokemon
-  })
-})
+app.get("/pokemon", (req, res) => {
+  res.render("index.ejs", { pokemon });
+});
 
-app.get('/pokemon/new', (req, res) => {
-  res.render('new.ejs');
-})
-
-app.post('/pokemon', (req, res) => {
+app.post("/pokemon", (req, res) => {
   pokemon.push(req.body);
+  res.redirect("/pokemon");
+});
+
+app.get("/pokemon/new", (req, res) => res.render("new.ejs"));
+
+app.get("/pokemon/:index", (req, res) => {
+  const index = req.params.index;
+  res.render("show.ejs", { pokemon: pokemon[index], index });
+});
+
+app.put("/pokemon/:index", (req, res) => {
+  const index = req.params.index;
+  pokemon[index] = req.body;
+  res.redirect("/pokemon");
+});
+
+app.delete("/pokemon/:index", (req, res) => {
+  const index = req.params.index;
+  pokemon.splice(index, 1);
   res.redirect('/pokemon');
-})
+});
 
-//show
-app.get('/pokemon/:index', (req, res) => {
-  console.log(req.params.index);
-  res.render('show.ejs', {
-      character: pokemon[req.params.index]
-  })
-})
+app.get("/pokemon/:index/edit", (req, res) => {
+  const index = req.params.index;
 
-app.delete('/pokemon/:index', (req, res) => {
-  pokemon.splice(req.params.index, 1);
-  res.redirect('/pokemon');
-})
+  res.render("edit.ejs", {
+    pokemon: pokemon[index],
+    index,
+  });
+});
 
-app.get('/pokemon/:index/edit', (req, res) => {
-  res.render('edit.ejs', {
-      character: pokemon[req.params.index],
-      characterIndex: req.params.index
-  })
-})
-
-app.put('/pokemon/:index', (req, res) => {
-  pokemon[req.params.index] = req.body;
-  res.redirect('/pokemon');
-})
-
-app.listen(3000);
-console.log('im listening');
+app.listen(3000, () => console.log("I'm listening!"));
